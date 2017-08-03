e962b4d6ef41c01cdd1e06e89ea8c8f5fa7db90c
upisfree <upisfree@outlook.com>
2017-08-04 01:40:09 +0600

    Second Unity experience (6 hours)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Item : MonoBehaviour {
+  public static int WIN_COUNT = 0;
+
+  private float X = GetRandomNumber(-1, 1);
+  private float Y = GetRandomNumber(-1, 1);
+  private float Z = GetRandomNumber(-1, 1);
+
+  public int Distance = 2;
+  public int Seconds = 0;
+  public bool IsAlive = true;
+
+  public InAudioNode music;
+  public InAudioNode voice;
+
+  public GameObject StartImage;
+  public GameObject EndImage;
+  public GameObject Canvas;
+  public GameObject Cam;
+
+  void Start () {
+    InAudio.Play(gameObject, music);
+
+    StartImage = GameObject.Find("Start");
+    EndImage = GameObject.Find("End");
+    Canvas = GameObject.Find("Canvas");
+    Cam = GameObject.Find("Main Camera");
+  }
+	
+	void Update () {
+    gameObject.transform.Rotate(X, Y, Z);
+
+    //if (StartImage.active)
+    //  StartImage.SetActive(false);
+    //else
+    //  StartImage.SetActive(true);
+
+    if (IsAlive)
+    {
+      if (Vector3.Distance(transform.position, GameObject.Find("NoClipFirstPersonController").transform.position) < Distance)
+      {
+        X /= 10;
+        Y /= 10;
+        Z /= 10;
+
+        StartCoroutine(KillObject());
+      }
+    }
+  }
+
+  IEnumerator KillObject()
+  {
+    IsAlive = false;
+    
+    yield return new WaitForSeconds(Seconds);
+  
+    InAudio.Play(gameObject, voice);
+    InAudio.Stop(gameObject, music);
+
+    WIN_COUNT += 1;
+    
+    if (WIN_COUNT > 10)
+    {
+      yield return new WaitForSeconds(8);
+
+      //GetComponent<NoClipFirstPersonController>().enabled = false;
+      //GetComponent<NoClipMouseLook>().enabled = false;
+
+      StartImage.SetActive(false);
+      Canvas.SetActive(true);
+      Cam.SetActive(false);
+
+      yield return new WaitForSeconds(15);
+      
+      WIN_COUNT = 0;
+
+      SceneManager.LoadScene("scene");
+    }
+  }
+
+  static float GetRandomNumber(double minimum, double maximum)
+  {
+    System.Random random = new System.Random();
+    return (float)(random.NextDouble() * (maximum - minimum) + minimum);
+  }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour {
+  public InAudioNode noise;
+
+  void Start () {
+    Cursor.visible = false;
+
+    InAudio.Play(gameObject, noise);
+
+    GetComponent<NoClipFirstPersonController>().enabled = false;
+    GetComponent<NoClipMouseLook>().enabled = false;
+
+    GameObject.Find("Start").SetActive(true);
+    
+    StartCoroutine(RemoveStartScreen());
+  }
+
+  void Update () {
+	}
+
+  IEnumerator RemoveStartScreen()
+  {
+    yield return new WaitForSeconds(12);
+    
+    GameObject.Find("Canvas").SetActive(false);
+
+    GetComponent<NoClipFirstPersonController>().enabled = true;
+    GetComponent<NoClipMouseLook>().enabled = true;
+  }
+}
